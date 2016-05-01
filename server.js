071c36f6cc9295b3e888cacad2b838d322725249
mikegking <mikegking@gmail.com>
2016-05-01 12:55:15 +1200

    added index and zipcode scripts

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ http.createServer(function(req,res){
   fs.readFile('index.html',function(err,data){
     res.writeHead(200,{'Content-type':'text/html'});
     res.end(data);
+    //console.log('Someone touched wep page!');
   });
 }).listen(1337,'127.0.0.1');
 console.log('Webserver has started');
